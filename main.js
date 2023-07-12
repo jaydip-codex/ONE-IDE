aa0fb5a3a0534bc9d92eed4d2709ce1e04764b5d
Patel Jaydip Naginbhai <jaydip.codex@gmail.com>
2023-07-12 10:17:06 +0530

    Initial Commit
    Features
    1.Online ide for JavaScript ,html and CSS. 
    2. Provide output in same browser window.
    3. With Multiple Theme/ Mode.

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// ----FUNCTION FOR CODE COMPILE AND OUTPUT---- 
+function run() {
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+    let output = document.getElementById("output");
+
+    output.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
+
+    output.contentWindow.eval(jsCode);
+}
+
+// ----FUNCTION TO CHANGE THEME----
+const changeTheme = (theme) => {
+    document.body.classList = ''
+    document.body.classList.add(theme)
+}
+
